e94ddfa8b86ca8ee61efa1f41d8ba494a7ef8e16
lockerprime <kannan.inbox2013@gmail.com>
2021-08-11 19:13:32 +0530

    form system

---
+++ outrealsrunityformsystem/Runtime/FormUiComponents.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+using System;
+
+public class FormUiComponents : MonoBehaviour
+{
+    public GameObject UiType;
+    public List<GameObject> UiComponents;
+    public Dictionary<InputField, string> InputField_Data = new Dictionary<InputField, string>();
+
+    public Dictionary<Slider, string> Slider_Data = new Dictionary<Slider, string>();
+    public Dictionary<Button, bool> Button_Data = new Dictionary<Button, bool>();
+    public Dictionary<Toggle, bool> Toggle_Data = new Dictionary<Toggle, bool>();
+    public Dictionary<Dropdown, int> Dropdown_Data = new Dictionary<Dropdown, int>();
+    public Dictionary<string, string> Unmatched_Data = new Dictionary<string, string>();
+    // Start is called before the first frame update
+
+    List<string> InputformData = new List<string>();
+
+    string filePath = Path.Combine(Application.streamingAssetsPath, "file.json");
+    public void SetForm()
+    {
+
+        serilaizedData playerInstance = new serilaizedData();
+        for (int i = 0; i < UiType.transform.childCount; i++)
+        {
+            UiComponents.Add(UiType.transform.GetChild(i).gameObject);
+        }
+
+
+        for (int i = 0; i < UiComponents.Count; i++)
+        {
+            if (UiComponents[i].GetComponent<InputField>() != null)
+            {
+                Debug.LogError("InputField");
+                InputField val = UiComponents[i].GetComponent<InputField>();
+                playerInstance.InputField = val.text;
+            }
+            else if (UiComponents[i].GetComponent<Slider>() != null)
+            {
+                Slider val = UiComponents[i].GetComponent<Slider>();
+               playerInstance.Slider = val.value.ToString();
+            }
+            else if (UiComponents[i].GetComponent<Button>() != null)
+            {
+                Button val = UiComponents[i].GetComponent<Button>();
+                playerInstance.Button = val.IsInteractable();
+            }
+            else if (UiComponents[i].GetComponent<Toggle>() != null)
+            {
+
+                Toggle val = UiComponents[i].GetComponent<Toggle>();
+                playerInstance.Toggle = val.isOn;
+            }
+            else if (UiComponents[i].GetComponent<Dropdown>() != null)
+            {
+
+                Dropdown val = UiComponents[i].GetComponent<Dropdown>();
+                playerInstance.Dropdown = val.value;
+            }
+            else
+            {
+                playerInstance.exception= "unmatched component";
+             
+            }
+        }
+
+    }
+    public void SavetoJson()
+    {
+ 
+
+        InputformData.Add(InputField_Data.ToString());
+        InputformData.Add(Slider_Data.ToString());
+        InputformData.Add(Button_Data.ToString());
+        InputformData.Add(Toggle_Data.ToString());
+        InputformData.Add(Dropdown_Data.ToString());
+        string json = JsonUtility.ToJson(InputField_Data, true);
+        File.WriteAllText(Application.dataPath + "/FormDataFile.json", json);
+#if UNITY_EDITOR
+
+        Debug.LogError("the path" + Application.dataPath);
+#endif
+#if UNITY_WEBGL
+        File.WriteAllText(filePath, json);
+#endif
+
+    }
+
+
+    [Serializable]
+    public class serilaizedData
+    {
+        public string InputField;
+        public string Slider;
+        public bool Button;
+        public bool Toggle;
+        public int Dropdown;
+        public string exception;
+
+    }
+}
