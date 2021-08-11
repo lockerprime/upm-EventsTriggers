a0efdbaad915b12b982e1079061f7770a4d8bc4a
lockerprime <kannan.inbox2013@gmail.com>
2021-08-11 20:12:59 +0530

    Update FormUiComponents.cs

--- outrealsrunityformsystem/Runtime/FormUiComponents.cs
+++ outrealsrunityformsystem/Runtime/FormUiComponents.cs
@@ -10,12 +10,7 @@ public class FormUiComponents : MonoBehaviour
     public GameObject UiType;
     public List<GameObject> UiComponents;
     public Dictionary<InputField, string> InputField_Data = new Dictionary<InputField, string>();
-
-    public Dictionary<Slider, string> Slider_Data = new Dictionary<Slider, string>();
-    public Dictionary<Button, bool> Button_Data = new Dictionary<Button, bool>();
-    public Dictionary<Toggle, bool> Toggle_Data = new Dictionary<Toggle, bool>();
-    public Dictionary<Dropdown, int> Dropdown_Data = new Dictionary<Dropdown, int>();
-    public Dictionary<string, string> Unmatched_Data = new Dictionary<string, string>();
+ 
     // Start is called before the first frame update
 
     List<string> InputformData = new List<string>();
@@ -25,8 +20,7 @@ public class FormUiComponents : MonoBehaviour
     public void SetForm()
     {
 
-    
-        for (int i = 0; i < UiType.transform.childCount; i++)
+         for (int i = 0; i < UiType.transform.childCount; i++)
         {
             UiComponents.Add(UiType.transform.GetChild(i).gameObject);
         }
@@ -38,43 +32,45 @@ public class FormUiComponents : MonoBehaviour
             {
                 Debug.LogError("InputField");
                 InputField val = UiComponents[i].GetComponent<InputField>();
-                playerInstance.InputField = val.text;
+                playerInstance.InputField.Add(val.text.ToString());  
             }
             else if (UiComponents[i].GetComponent<Slider>() != null)
             {
                 Slider val = UiComponents[i].GetComponent<Slider>();
-               playerInstance.Slider = val.value.ToString();
+                playerInstance.Slider.Add(val.value.ToString());
+                
             }
             else if (UiComponents[i].GetComponent<Button>() != null)
             {
                 Button val = UiComponents[i].GetComponent<Button>();
-                playerInstance.Button = val.IsInteractable();
+                playerInstance.Button.Add(val.IsInteractable());
+           
             }
             else if (UiComponents[i].GetComponent<Toggle>() != null)
             {
 
                 Toggle val = UiComponents[i].GetComponent<Toggle>();
-                playerInstance.Toggle = val.isOn;
+                playerInstance.Toggle.Add(val.isOn);
+     
             }
             else if (UiComponents[i].GetComponent<Dropdown>() != null)
             {
 
                 Dropdown val = UiComponents[i].GetComponent<Dropdown>();
-                playerInstance.Dropdown = val.value;
+                playerInstance.Dropdown.Add(val.value);
+
+               
             }
             else
             {
-                playerInstance.exception= "unmatched component";
-             
+                playerInstance.exception.Add("unmatched component");
             }
         }
         SavetoJson();
     }
     public void SavetoJson()
     {
-
-
-        string json = JsonUtility.ToJson(playerInstance, true);
+         string json = JsonUtility.ToJson(playerInstance, true);
         Debug.LogError("the path" + Application.dataPath);
         File.WriteAllText(Application.dataPath + "/FormDataFile.json", json);
 #if UNITY_EDITOR
@@ -91,12 +87,13 @@ public class FormUiComponents : MonoBehaviour
     [Serializable]
     public class serilaizedData
     {
-        public string InputField;
-        public string Slider;
-        public bool Button;
-        public bool Toggle;
-        public int Dropdown;
-        public string exception;
+        public List<string> InputField= new List<string>();
+        public List<string> Slider = new List<string>();
+        public List<bool> Button = new List<bool>();
+        public List<bool> Toggle = new List<bool>();
+        public List<int> Dropdown = new List<int>();
+        public List<string> exception = new List<string>();
+ 
 
     }
 }
